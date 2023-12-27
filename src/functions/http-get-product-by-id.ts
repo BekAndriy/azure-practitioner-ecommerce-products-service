19e83681bb392f1dda668c9e0ac6d647ed888dc5
Andriy <andriybek@gmail.com>
2023-12-27 12:31:18 +0200

    Task-4 No SQL integration

--- src/functions/http-get-product-by-id.ts
+++ src/functions/http-get-product-by-id.ts
@@ -1,14 +1,47 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 import { errorResponse, successJSONResponse } from "../utils/response";
-import { products } from '../mocks/data';
-
-const getProductById = (productId: string) => productId && products.find((product) => product.id === productId);
+import { logParameters } from "../utils/logger";
+import { Client } from "../db";
 
 export const httpGetProductById = async (request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> => {
-  const { productId } = request.params
-  const product = getProductById(productId);
+  logParameters(request, context);
 
-  return product ? successJSONResponse(product) : errorResponse(404, 'Product not found');
+  const { productId } = request.params;
+
+  const [products, stocks] = await Promise.all([
+    Client.container('products')
+      .items
+      .query({
+        query: 'SELECT * FROM products p WHERE p.id = @id',
+        parameters: [{ name: '@id', value: productId }]
+      })
+      .fetchAll(),
+    Client.container('stocks')
+      .items
+      .query({
+        query: 'SELECT * FROM stocks s WHERE s.product_id = @productId',
+        parameters: [
+          {
+            name: '@productId',
+            value: productId
+          }
+        ]
+      })
+      .fetchAll()
+  ]);
+
+  const [product] = products.resources;
+
+  if (!product) {
+    return errorResponse('Product not found', 404);
+  }
+
+  const { id, price, title, description } = product;
+  const { count } = stocks.resources[0];
+  const productResult = {
+    id, price, title, description, count
+  };
+  return successJSONResponse(productResult);
 };
 
 app.http('http-get-product-by-id', {
